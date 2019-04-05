89922493b9d35ab8f0a7fe230ea81877b98bbe7a
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-05 09:37:54 -0700

    Added Routes and NavLink in nav. Finished MVPs. Need to style.

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
+import { Route, NavLink } from "react-router-dom";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -17,34 +18,51 @@ class App extends Component {
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
   // GET data
-  componentDidMount(){
-    axios.get("http://localhost:3333/smurfs")
-      .then(res => {
-        console.log(res.data);
-        this.setState({ smurfs:res.data});
-      });
-  };
+  componentDidMount() {
+    axios.get("http://localhost:3333/smurfs").then(res => {
+      console.log(res.data);
+      this.setState({ smurfs: res.data });
+    });
+  }
 
   // POST data
   addSmurf = (event, smurf) => {
     event.preventDefault();
-    axios.post("http://localhost:3333/smurfs", smurf)
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
       .then(res => {
         console.log(res.data);
-        this.setState({ smurfs:res.data });
-        alert('Congrats! Welcome to the village!');
+        this.setState({ smurfs: res.data });
+        alert(`Congrats! You've added ${smurf.name} to the village!`);
         // redirect to home
+        this.props.history.push("/");
       })
       .catch(err => {
-        console.log(err.resolve)
-      })
+        console.log(err.resolve);
+      });
   };
 
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav>
+          <NavLink to="/" activeClassName="active-nav">
+            Village
+          </NavLink>
+          <NavLink to="/smurf-form" activeClassName="active-nav">
+            Add Smurf
+          </NavLink>
+        </nav>
+        <Route
+          exact
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+
+        <Route
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} addSmurf={this.addSmurf} />}
+        />
       </div>
     );
   }
